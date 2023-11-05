9ae117288cca69c0cfc2d831ddef32bcab4b2212
hanunsusanti <21081010296@student.upnjatim.ac.id>
2023-11-05 12:20:27 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const req = require('express/lib/request');
+const path =  require('path');
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname)+'/views/index.html');    
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, _ => {
+    console.log(`App deployed at Port ${PORT}`);
+});
